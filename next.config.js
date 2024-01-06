bd711089b77f8c0bb266de114e6e705c7ea6fdd8
Gacadeau <gashimwea@gmail.com>
2024-01-06 12:27:30 +0200

    first commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
+
+const withOffline = require('next-offline');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  // Autres configurations Next.js ici...
 
-module.exports = nextConfig
+  // Configuration du service worker avec Workbox
+  workbox: {
+    // Gestion de la génération du service worker
+    generateInDevMode: true,
+    // Exclure les pages API du caching
+    exclude: [/\/api\//],
+    // Chemin vers votre fichier service worker personnalisé
+    swSrc: './public/service-worker.js', // Assurez-vous que le chemin est correct
+  },
+};
+
+module.exports = withOffline(nextConfig);
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,62 @@
 // pages/index.js
 
+import { useEffect, useState } from 'react';
 import VideoPlayer from './components/VideoPlayer';
 import VideoDownloadButton from './components/VideoDownloadButton';
+import { useRouter } from 'next/router';
 
 const Home = () => {
+  const [isOnline, setIsOnline] = useState(true);
+  const router = useRouter();
+
+  useEffect(() => {
+    // Vérifier la connectivité réseau
+    const handleOnlineStatusChange = () => {
+      setIsOnline(navigator.onLine);
+    };
+
+    window.addEventListener('online', handleOnlineStatusChange);
+    window.addEventListener('offline', handleOnlineStatusChange);
+
+    return () => {
+      window.removeEventListener('online', handleOnlineStatusChange);
+      window.removeEventListener('offline', handleOnlineStatusChange);
+    };
+  }, []);
+
   const videoUrl = '/dance.mp4'; // Chemin vers votre vidéo dans le dossier "public"
+
+  const handleDownloadClick = async () => {
+    try {
+      const registration = await navigator.serviceWorker.ready;
+      const response = await fetch(videoUrl);
+      const blob = await response.blob();
+
+      // Envoyer un message au service worker pour stocker la vidéo en cache
+      registration.active.postMessage({
+        type: 'CACHE_VIDEO',
+        url: videoUrl,
+        blob,
+      });
+
+      console.log('Video ajoutée au cache avec succès.');
+    } catch (error) {
+      console.error('Erreur lors du téléchargement de la vidéo:', error);
+    }
+  };
+
+  useEffect(() => {
+    if (!isOnline) {
+      // Rediriger vers la page des vidéos en cache si l'utilisateur est hors ligne
+      router.push('/cached-videos');
+    }
+  }, [isOnline]);
 
   return (
     <div className="container mx-auto mt-8 text-center">
       <h1 className="text-3xl font-bold mb-4">Bienvenue sur la page d'accueil</h1>
       <VideoPlayer videoUrl={videoUrl} />
-      <VideoDownloadButton videoUrl={videoUrl} />
+      <VideoDownloadButton videoUrl={videoUrl} onClick={handleDownloadClick} />
     </div>
   );
 };

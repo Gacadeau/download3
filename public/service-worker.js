f65a1a6bdc7ee53122a87a1e0e41b085d505dc03
Gacadeau <gashimwea@gmail.com>
2024-01-08 09:20:40 +0200

    first commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,25 @@
 // public/service-worker.js
 
 self.addEventListener('message', (event) => {
+  console.log('Message received:', event.data);
+
   if (event.data && event.data.type === 'CACHE_VIDEO') {
     const { url, blob } = event.data;
 
     caches.open('downloaded-videos-cache').then((cache) => {
       cache.put(url, new Response(blob));
+      console.log(`Video cached: ${url}`);
     });
   }
 });
 
 self.addEventListener('fetch', (event) => {
-  // Mettre en cache les vidéos provenant de votre API ou serveur
   if (event.request.url.endsWith('.mp4')) {
     event.respondWith(
       caches.match(event.request).then((response) => {
-        return response || fetch(event.request);
+        const fetched = fetch(event.request);
+        console.log(`Video fetched: ${event.request.url}`);
+        return response || fetched;
       })
     );
   }
